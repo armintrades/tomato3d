8778d74c5fec9a092bd406afc4120755d0129364
arminnqv <arminmqv@gmail.com>
2026-06-27 23:38:57 +0330

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tomato3D"
+include(":app")
